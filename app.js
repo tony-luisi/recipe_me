7e869ac75ba58943d61ac2e80a26d481613fdbba
tony-luisi <t.luisi@gmail.com>
2016-05-24 18:44:42 +1200

    tidied up code

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ var request = require('superagent')
 
 var app = express();
 
-// var recipeJSON = JSON.parse(recipe)
-// console.log(recipe)
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -17,22 +13,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//---------------------Ignore above here-------------------//
-// var recipe = {
-//       "publisher": "Simply Recipes",
-//       "f2f_url": "http://food2fork.com/view/36251",
-//       "title": "Easy Brazilian Cheese Bread",
-//       "source_url": "http://www.simplyrecipes.com/recipes/easy_brazilian_cheese_bread/",
-//       "recipe_id": "36251",
-//       "image_url": "http://static.food2fork.com/braziliancheesebreada300x200ffd79a7b.jpg",
-//       "social_rank": 100,
-//       "publisher_url": "http://simplyrecipes.com"
-//     }
-
-
-
 app.get('/', function(req, res) {
- res.redirect('/search') // what is this doing?
+ res.redirect('/search') //redirects to the search page
 })
 
 app.get('/search', function(req, res) {
@@ -47,13 +29,12 @@ app.post('/results', function(req,res) {
     ingredients[2] = "marmite"
   }
 
-  var query = escape(ingredients[0] + ','+ ingredients[1]+','+ingredients[2])
+  var query = escape(ingredients[0] + ','+ ingredients[1]+','+ingredients[2]) //to escape any insecure text!!
 
   request.get('http://food2fork.com/api/search?key=d38b86e56e463fc2a8efb429bf1a0992&q='+query)
   .set('Accept','application/json')
   .end(function(err,response){
     var recipeResponse = JSON.parse(response.text)
-    console.log(recipeResponse.recipes[0], "reciperesponse..................")
     recipeResponse.recipes[0].title = recipeResponse.recipes[0].title.replace(/&amp;/g, '&')
     res.render('results', recipeResponse.recipes[0])
   })
